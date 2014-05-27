bc5133717ad565c06590f5cb8b9f72a6354b3ed8
will hunting <willhunting@wills-Mac.local>
2014-05-27 22:17:32 +0800

    Initial Commit

---
+++ Attributor/HWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HWAppDelegate.h
+//  Attributor
+//
+//  Created by will hunting on 5/27/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Attributor/HWViewController.h
@@ -0,0 +1,13 @@
+//
+//  HWViewController.h
+//  Attributor
+//
+//  Created by will hunting on 5/27/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HWViewController : UIViewController
+
+@end
